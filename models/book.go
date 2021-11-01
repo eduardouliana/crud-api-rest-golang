d1654322f8839523689c5b93ac46896a4df3746b
Eduardo Uliana <edu02_rko@hotmail.com>
2021-11-01 20:36:18 -0300

    Add struct of book

---
+++ models/book.go
@@ -0,0 +1,16 @@
+package models
+
+import (
+	"time"
+)
+
+type Book struct {
+	ID          uint      `json:"id" gorm:"primaryKey"`
+	Name        string    `json:""`
+	Description string    `json:"description"`
+	MediumPrice string    `json:"medium_price"`
+	Author      string    `json:"author"`
+	ImageURL    string    `json:"img_url"`
+	CreatedAt   time.Time `json:"created"`
+	UpdatedAt   time.Time `json:"updated"`
+}
